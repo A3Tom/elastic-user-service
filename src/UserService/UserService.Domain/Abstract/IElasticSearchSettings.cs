225f754fb859f25f4bc973e6086997a516f2d40c
A3Tom <Tom_Esq@Yahoo.com>
2023-02-05 23:08:53 +0000

    Added a domain layer for settings

---
+++ src/UserService/UserService.Domain/Abstract/IElasticSearchSettings.cs
@@ -0,0 +1,6 @@
+﻿namespace UserService.Domain.Abstract;
+public interface IElasticSearchSettings
+{
+    string ElasticSearchNodeUrl { get; init; }
+    string ElasticSearchDefaultIndex { get; init; }
+}
---
+++ src/UserService/UserService.Domain/Settings/UserServiceSettings.cs
@@ -0,0 +1,8 @@
+﻿using UserService.Domain.Abstract;
+
+namespace UserService.Domain.Settings;
+public class UserServiceSettings: IElasticSearchSettings
+{
+    public string ElasticSearchNodeUrl { get; init; } = string.Empty;
+    public string ElasticSearchDefaultIndex { get; init; } = string.Empty;
+}
