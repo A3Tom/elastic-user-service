62d9327187e448b0e38f4ebd069048d191482418
A3Tom <Tom_Esq@Yahoo.com>
2023-02-05 21:39:44 +0000

    Added User entity & view model

---
+++ src/UserService/UserService.Domain/Entities/User.cs
@@ -0,0 +1,6 @@
+﻿namespace UserService.Domain.Entities;
+
+#pragma warning disable IDE1006 // Naming Styles - Source data name is immutable
+public record User(string first_name, string last_name, string gender, string email);
+#pragma warning restore IDE1006 // Naming Styles
+
---
+++ src/UserService/UserService.Domain/ViewModels/UserVM.cs
@@ -0,0 +1,20 @@
+﻿using UserService.Domain.Entities;
+
+namespace UserService.Api.ViewModels;
+
+public class UserVM
+{
+    public string FirstName { get; init; } = string.Empty;
+    public string Surname { get; init; } = string.Empty;
+    public string? EmailAddress { get; init; }
+    public string? Gender { get; init; }
+
+    public static explicit operator UserVM(User entity)
+        => new()
+        {
+            FirstName = entity.first_name,
+            Surname = entity.last_name,
+            EmailAddress = entity.email,
+            Gender = entity.gender
+        };
+}
