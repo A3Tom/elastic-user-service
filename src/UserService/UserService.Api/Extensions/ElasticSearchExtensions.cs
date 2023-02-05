96bda711d6641a931727bc4836d3d6882315b765
A3Tom <Tom_Esq@Yahoo.com>
2023-02-05 23:10:34 +0000

    Added a handy extension for wiring up ElasticSearch

---
+++ src/UserService/UserService.Api/Extensions/ElasticSearchExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Nest;
+using UserService.Domain.Abstract;
+
+namespace UserService.Api.Extensions;
+
+public static class ElasticSearchExtensions
+{
+    public static void AddUserElasticSearch(this IServiceCollection services)
+    {
+        var settings = services
+            .BuildServiceProvider()
+            .GetRequiredService<IElasticSearchSettings>();
+
+        var connectionSettings = new ConnectionSettings(new Uri(settings.ElasticSearchNodeUrl))
+            .DefaultIndex(settings.ElasticSearchDefaultIndex);
+        var elasticClient = new ElasticClient(connectionSettings);
+
+        services.AddScoped<IElasticClient>(x => elasticClient);
+    }
+}
