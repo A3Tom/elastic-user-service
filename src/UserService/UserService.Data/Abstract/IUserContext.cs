74c5a9a3da2aec141335446a197393f71b9794ed
A3Tom <Tom_Esq@Yahoo.com>
2023-02-05 23:11:31 +0000

    We now have a user context to query against !

---
+++ src/UserService/UserService.Data/Abstract/IUserContext.cs
@@ -0,0 +1,7 @@
+﻿using UserService.Api.ViewModels;
+
+namespace UserService.Data.Abstract;
+public interface IUserContext
+{
+    Task<IEnumerable<UserVM>> SearchForUser(string searchTerm);
+}
---
+++ src/UserService/UserService.Data/Contexts/UserContext.cs
@@ -0,0 +1,40 @@
+﻿using Nest;
+using UserService.Api.ViewModels;
+using UserService.Data.Abstract;
+using UserService.Domain.Entities;
+
+namespace UserService.Data.Contexts;
+public class UserContext : IUserContext
+{
+    private readonly IElasticClient _client;
+
+    private readonly string[] _searchFields = {
+        "first_name",
+        "last_name",
+        "email"
+    };
+
+    public UserContext()
+    {
+        var settings = new ConnectionSettings(new Uri("http://localhost:9200"))
+            .DefaultIndex("users");
+        _client = new ElasticClient(settings);
+    }
+
+    public async Task<IEnumerable<UserVM>> SearchForUser(string searchTerm)
+    {
+        var queryResult = await _client.SearchAsync<User>(x => x
+            .Index("users")
+            .Query(q => q
+                .QueryString(qs => qs
+                    .Query($"*{searchTerm}*")
+                    .Fields(f => f.Fields(_searchFields))
+                )
+            )
+            .TrackScores());
+
+        return queryResult.Hits
+            .Where(x => x.Score == queryResult.MaxScore)
+            .Select(x => (UserVM)x.Source);
+    }
+}
