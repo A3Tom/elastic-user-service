8a9921e3863b96344f43d056dce5f9d6e933080d
A3Tom <Tom_Esq@Yahoo.com>
2023-02-05 23:11:17 +0000

    Added Commands & Queries projects

---
+++ src/UserService/UserService.Application/UseCases/Users/Handlers/SearchUsers.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using UserService.Api.ViewModels;
+using UserService.Data.Abstract;
+
+namespace UserService.Application.Queries.UseCases.Users.Handlers;
+public class SearchUsers : IRequestHandler<SearchUsers.Request, IEnumerable<UserVM>>
+{
+    private readonly IUserContext _userContext;
+
+    public SearchUsers(IUserContext userContext)
+    {
+        _userContext = userContext;
+    }
+
+    public async Task<IEnumerable<UserVM>> Handle(Request request, CancellationToken cancellationToken)
+    {
+        return await _userContext.SearchForUser(request.SearchTerm);
+    }
+
+    public class Request : IRequest<IEnumerable<UserVM>>
+    { 
+        public string? SearchTerm { get; init; }
+
+        public Request(string? searchTerm)
+        {
+            SearchTerm = searchTerm;
+        }
+    }
+}
